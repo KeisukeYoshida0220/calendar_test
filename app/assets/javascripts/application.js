6842bcb00043ef1c417b7b5d4d81c97ce9a31b3b
KeisukeYoshida0220 <y.keisuke0220@gmail.com>
2019-11-06 16:34:15 +0900

    create calender easy app

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,79 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require jquery
+//= require moment
+//= require fullcalendar
+//= require fullcalendar/lang/ja
+
+$(function() { // document ready
+
+  var calendar = $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'agendaWeek,agendaDay'
+    },
+    defaultView: 'agendaWeek',
+    defaultTimedEventDuration: '01:00',
+    allDaySlot: false,
+    scrollTime: '08:00',
+    businessHours: {
+      start: '9:00',
+      end: '18:00',
+    },
+    lang: /^en-/.test(navigator.language) ? 'en' : 'zh-cn',
+    eventOverlap: function(stillEvent, movingEvent) {
+      return true;
+    },
+    events: [{
+      title: 'hoge',
+      start: '2019-11-06T15:00+08:00'
+    }, {
+      title: 'hoge',
+      start: '2019-11-06T12:00+08:00'
+    }],
+    editable: true,
+    selectable: true,
+    selectHelper: true,
+    select: function(start, end) {
+      var duration = (end - start) /1000;
+      if(duration == 1800) {
+        // set default duration to 1 hr.
+      	end = start.add(30, 'mins');
+        return calendar.fullCalendar('select', start, end);
+      }
+      var title = prompt('Event Title:');
+      var eventData;
+      if (title && title.trim()) {
+        eventData = {
+          title: title,
+          start: start,
+          end: end
+        };
+        calendar.fullCalendar('renderEvent', eventData);
+      }
+      calendar.fullCalendar('unselect');
+    },
+    eventRender: function(event, element) {
+      var start = moment(event.start).fromNow();
+      element.attr('title', start);
+    },
+    loading: function() {
+    }
+  });
+
+});
